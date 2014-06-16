e530509b804467876b4758dac6e79747df78f77b
Faiz Shukri <faizshukri90@gmail.com>
2014-06-16 11:34:03 +0800

    Add angular loading bar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@
 angular
   .module('fasyaApp', [
     'ngRoute',
+    'angular-loading-bar',
     'ngAnimate'
   ])
   .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
