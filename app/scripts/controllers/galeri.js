387062e66f54ca186bc76503114010c6a8e47835
Faiz Shukri <faizshukri90@gmail.com>
2014-06-16 22:52:12 +0800

    Add galeri item

--- app/scripts/controllers/galeri.js
+++ app/scripts/controllers/galeri.js
@@ -8,6 +8,37 @@
  * Controller of the fasyaApp
  */
 angular.module('fasyaApp')
-  .controller('GaleriCtrl', function ($scope) {
+  .controller('GaleriCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $timeout(function(){
+        var api = jQuery('.megafolio-container').megafoliopro({
+            filterChangeAnimation:"rotatescale",
+            filterChangeSpeed:600,
+            filterChangeRotate:99,
+            filterChangeScale:0.6,          
+            delay:20,
+            paddingHorizontal:10,
+            paddingVertical:10,
+            layoutarray:[0]
+        });
 
-  });
+        jQuery(".fancybox").fancybox({
+            openEffect  : 'none',
+            closeEffect : 'none',
+            helpers : {
+                media : {}
+            }
+        });
+
+        jQuery('#galeri .filter').click(function() {            
+            api.megafilter(jQuery(this).data('category'));  
+            $.waypoints('refresh');
+        });
+
+        jQuery('.filter-list li').click(function(){
+            $('.filter-list li').each(function() { 
+                $(this).removeClass("selected")
+            });
+            $(this).addClass("selected");
+        });
+    });
+  }]);
