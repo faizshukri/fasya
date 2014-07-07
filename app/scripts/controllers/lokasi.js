0ae2a370985a953619ef8b9327d1fcd77262798c
Faiz Shukri <faizshukri90@gmail.com>
2014-07-07 09:36:18 +0800

    Create lokasi

--- app/scripts/controllers/lokasi.js
+++ app/scripts/controllers/lokasi.js
@@ -10,4 +10,18 @@
 angular.module('fasyaApp')
   .controller('LokasiCtrl', function ($scope) {
 
+    $scope.init = function(){
+        angular.element('#lokasi').delegate('.nav-tabs', 'click', function(e){
+            e.preventDefault();
+        });
+
+        angular.element('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
+            var iframe = $( $(e.target).attr('href') ).find('iframe');
+            var src = iframe.attr('src');
+            var style = iframe.attr('style');
+            iframe.parent().html('<iframe src="'+src+'" style="'+style+'" frameborder="0"></iframe>');
+        });
+    }
+
+    $scope.init();
   });
