95dbb49300fd4b70ae4c74d4c3e290361d696c32
Faiz Shukri <faizshukri90@gmail.com>
2014-07-09 17:57:57 +0800

    Refactor music play pause

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,37 +10,52 @@
 angular.module('fasyaApp')
   .controller('MainCtrl', function ($scope, $timeout) {
 
-    $scope.music = {};
+    $scope.music = {
+        played: false,
+        started: false,
+        lyc_init: false,
+        button: angular.element('.player_control').find('a')
+    };
 
     $scope.music.play = function(){
-        $scope.music.loadMusic(function(){
+        $scope.music.loadLyric(function(){
             $timeout(function(){
                 angular.element('#music')[0].play();
-                $scope.music.played = true;
-
-                angular.element('.player_control').find('a').click(function(e){
-                    e.preventDefault();
-                    $scope.music.togglePause( $(this) );
-                }).html('<i class="glyphicon glyphicon-pause"></i>');
-
+                if(!$scope.music.started){
+                    $scope.music.started = true;
+                    $scope.music.toggle();
+                }
             }, 1000);
+            angular.element('.sub_text').fadeIn();
         });
     }
 
-    $scope.music.togglePause = function(button){
-        if($scope.music.played){
+    $scope.music.toggle = function(){
+
+        // Not started yet
+        if(!$scope.music.started){
+            $scope.music.play();
+
+        // Played, wanna pause
+        } else if( $scope.music.played ){
             angular.element('#music')[0].pause();
             $scope.music.lyric.pauseToggle();
-            button.html('<i class="glyphicon glyphicon-play"></i>');
+            $scope.music.played = false;
+            angular.element('.sub_text').fadeOut();
+            $scope.music.button.html('<i class="glyphicon glyphicon-play"></i>');
+
+        // Paused, wanna play
         } else {
             angular.element('#music')[0].play();
-            $scope.music.lyric.pauseToggle();
-            button.html('<i class="glyphicon glyphicon-pause"></i>');
+            if( $scope.music.lyc_init ) $scope.music.lyric.pauseToggle();
+            else $scope.music.lyc_init = true;
+            $scope.music.played = true;
+            angular.element('.sub_text').fadeIn();
+            $scope.music.button.html('<i class="glyphicon glyphicon-pause"></i>');
         }
-        $scope.music.played = !$scope.music.played;
     }
 
-    $scope.music.loadMusic = function(callback){
+    $scope.music.loadLyric = function(callback){
         jQuery.ajax('music/lrc/yaro.lrc').done(function(value){
             $scope.music.lyric = new Lrc(value, $scope.music.outputHandler);
             $scope.music.lyric.play();
@@ -49,11 +64,14 @@ angular.module('fasyaApp')
     }
 
     $scope.music.outputHandler = function(line, extra){
-        jQuery('.sub_text span').text(line);
+        angular.element('.sub_text span').text(line);
     }
 
     $scope.init = function(){
-        $scope.music.play();
+        $scope.music.button.click(function(e){
+            e.preventDefault();
+            $scope.music.toggle();
+        });
     };
 
     $scope.init();
