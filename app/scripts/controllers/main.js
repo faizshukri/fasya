7d3fcbe1ef244fa3016d8b4c4de9005a96314805
Faiz Shukri <faizshukri90@gmail.com>
2014-07-13 09:19:08 +0800

    Better music experience

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,85 +11,77 @@ angular.module('fasyaApp')
   .controller('MainCtrl', function ($scope, $timeout, $modal) {
 
     $scope.music = {
-        played: false,
-        started: false,
-        lyc_init: false,
-        button: angular.element('.player_control').find('a'),
-        mp3_element: angular.element('#music'),
+        elem_button: angular.element('.player_control').find('a'),
+        elem_mp3: angular.element('#music'),
+        elem_sub: angular.element('.sub_text span'),
         tracks: [
             'youaretheone',
             'thisring'
         ],
-        nowplaying: 0
+        track_no: 0,
+        lyric_line: 0,
+        isPlay: false
     };
 
-    $scope.music.play = function(){
-        $scope.music.loadLyric(function(){
-            $timeout(function(){
-                $scope.music.mp3_element[0].play();
-                if(!$scope.music.started){
-                    $scope.music.started = true;
-                    $scope.music.toggle();
-                }
-            }, 1000);
-            angular.element('.sub_text').fadeIn();
-        });
-    }
-
-    $scope.music.toggle = function(){
-
-        // Not started yet
-        if(!$scope.music.started){
-            $scope.music.play();
-
-        // Played, wanna pause
-        } else if( $scope.music.played ){
-            $scope.music.mp3_element[0].pause();
-            $scope.music.lyric.pauseToggle();
-            $scope.music.played = false;
-            angular.element('.sub_text').fadeOut();
-            $scope.music.button.html('<i class="glyphicon glyphicon-play"></i>');
-
-        // Paused, wanna play
-        } else {
-            $scope.music.mp3_element[0].play();
-            if( $scope.music.lyc_init ) $scope.music.lyric.pauseToggle();
-            else $scope.music.lyc_init = true;
-            $scope.music.played = true;
-            angular.element('.sub_text').fadeIn();
-            $scope.music.button.html('<i class="glyphicon glyphicon-pause"></i>');
-        }
-    }
-
     $scope.music.loadLyric = function(callback){
-        jQuery.ajax('music/lrc/'+$scope.music.tracks[ $scope.music.nowplaying ]+'.lrc').done(function(value){
-            $scope.music.lyric = new Lrc(value, $scope.music.outputHandler);
-            $scope.music.lyric.play();
+        jQuery.ajax('music/lrc/'+$scope.music.tracks[ $scope.music.track_no ]+'.lrc').done(function(value){
+            $scope.music.lyric = new Lrc(value);
+            $scope.music.elem_sub.text($scope.music.lyric.tags.artist +' - '+ $scope.music.lyric.tags.title);
+            // $scope.music.lyric.play();
             if(callback) callback();
         });
     }
 
-    $scope.music.outputHandler = function(line, extra){
-        angular.element('.sub_text span').text(line);
+    $scope.music.loadMusic = function(){
+        $scope.music.elem_mp3.find('source').attr('src', 'music/mp3/'+$scope.music.tracks[ $scope.music.track_no ]+'.mp3');
+        $scope.music.elem_mp3[0].pause();
+        $scope.music.elem_mp3[0].load();
     }
 
-    $scope.music.preload = function(){
-        $scope.music.mp3_element.find('source').attr('src', 'music/mp3/'+$scope.music.tracks[ $scope.music.nowplaying ]+'.mp3');
-        $scope.music.mp3_element[0].pause();
-        $scope.music.mp3_element[0].load();
+    $scope.music.togglePlay = function(){
+        if($scope.music.isPlay) $scope.music.elem_mp3[0].pause();
+        else $scope.music.elem_mp3[0].play();
     }
 
     $scope.init = function(){
+        $scope.music.loadLyric();
+        $scope.music.loadMusic();
 
-        $scope.music.preload();
-
-        // Make audio buffer as fast as it can 
-        $scope.music.mp3_element[0].play();
-        $scope.music.mp3_element[0].pause();
-
-        $scope.music.button.click(function(e){
+        $scope.music.elem_button.click(function(e){
             e.preventDefault();
-            $scope.music.toggle();
+            $scope.music.togglePlay();
+        });
+
+        $scope.music.elem_mp3.on('play', function(e){
+            angular.element('.sub_text').fadeIn();
+            $scope.music.elem_button.html('<i class="glyphicon glyphicon-pause"></i>');
+            $scope.music.isPlay = true;
+        });
+        $scope.music.elem_mp3.on('pause', function(e){
+            angular.element('.sub_text').fadeOut();
+            $scope.music.elem_button.html('<i class="glyphicon glyphicon-play"></i>');
+            $scope.music.isPlay = false;
+        });
+        $scope.music.elem_mp3.on('timeupdate', function(e){
+            var musictime = parseInt(this.currentTime.toFixed(3).split('.').join(''));
+            var subtime = $scope.music.lyric.lines[ $scope.music.lyric_line ];
+
+            if(subtime && musictime >= subtime.time - 1000){
+                $scope.music.elem_sub.text(subtime.txt);
+                $scope.music.lyric_line++;
+                while( $scope.music.lyric_line < ( $scope.music.lyric.lines.length - 1) && $scope.music.lyric.lines[$scope.music.lyric_line + 1].time < musictime){
+                    $scope.music.lyric_line++;
+                }
+            }
+        });
+
+        $scope.music.elem_mp3.on('ended', function(){
+            $scope.music.isPlay = false;
+            $scope.music.lyric_line = 0;
+            $scope.music.track_no = ( $scope.music.track_no + 1 ) % $scope.music.tracks.length;
+            $scope.music.loadLyric();
+            $scope.music.loadMusic();
+            $scope.music.togglePlay();
         });
 
         var modal = $modal({
@@ -105,25 +97,11 @@ angular.module('fasyaApp')
         
         modal.$promise.then(function(){
             modal.$scope.$on('modal.hide', function(){
-                $scope.music.toggle();
+                $scope.music.togglePlay();
             });
         });
-
-        $scope.music.mp3_element.on('ended', function(){
-            $scope.music.button.html('<i class="glyphicon glyphicon-play"></i>');
-            $scope.music.played = false;
-            $scope.music.started = false;
-            $scope.music.lyc_init = false;
-            $scope.music.lyric.stop();
-            $scope.music.nowplaying = ($scope.music.nowplaying + 1) % $scope.music.tracks.length;
-            $scope.music.preload();
-            $scope.music.toggle();
-            console.log('finish play');
-        });
-    };
+    }
 
     $scope.init();
-
-    
 
   });
