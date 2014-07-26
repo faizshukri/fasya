5e650b2aee3e8454763d49374809f399b57e131b
Faiz Shukri <faizshukri90@gmail.com>
2014-07-26 15:16:18 +0800

    Add contact page and fix navigation in mobile

--- app/scripts/controllers/hubungi.js
+++ app/scripts/controllers/hubungi.js
@@ -8,6 +8,57 @@
  * Controller of the fasyaApp
  */
 angular.module('fasyaApp')
-  .controller('HubungiCtrl', function ($scope) {
+  .controller('HubungiCtrl', function ($scope, $timeout) {
+
+    $scope.contact = {};
+
+    $scope.send = false;
+
+    angular.element('#sendMessage').on('click', function(e){
+
+        e.preventDefault();
+
+        $scope.send = true;
+
+        if($scope.hubungiForm.$invalid){
+            angular.element('input.ng-invalid, textarea.ng-invalid').first().focus();
+            $scope.$apply();
+            return;
+        }
+
+        var subject = '[Fasya] ';
+
+        if($scope.contact.kad_kenduri){
+            subject += 'Kad Kenduri, ';
+        }
+
+        if($scope.contact.soalan){
+            subject += 'Soalan, ';
+        }
+
+        subject += 'mesej daripada '+$scope.contact.name;
+        $.ajax({
+            url: 'http://mail.fasya.com',
+            data: { 
+                from: $scope.contact.name + ' <' + $scope.contact.email + '>',
+                to: 'Faiz Shukri <ukhwahfillah90@gmail.com>', 
+                subject: subject, 
+                text: $scope.contact.message 
+            },
+            type: 'POST',
+            dataType: 'json',
+            beforeSend: function(){
+                $('#sendStatus').text('sending..').fadeIn();
+            }
+        }).done(function(data){
+            $('#sendStatus').text('your message is successfully sent ');
+            $timeout(function(){
+                $('#sendStatus').fadeOut();
+            }, 2000);
+            $('input,textarea').val('');
+            $('input[type=checkbox]').attr('checked', false)
+            $scope.send = true;
+        });
+    });
 
   });
